ada68365d3e6e719137ffc3b77f0d58128903491
Phil Boyd <phil.boyd@prb-services-llc.com>
2022-02-10 09:46:05 -0500

    wip(react-tcg): new module

---
+++ apps/react-tcg-06/src/main.js
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom';
+import './main.css';
+import App from './app/App';
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
+);
